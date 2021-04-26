e5bef1f0e8e0e05f11bdab4241830253b838a71c
collinsnji <collins.geek@gmail.com>
2021-04-25 21:05:41 -0400

    Remove env vars and bug fixes

--- src/main/java/com/sps/utils/PoemTextToSpeech.java
+++ src/main/java/com/sps/utils/PoemTextToSpeech.java
@@ -1,18 +1,27 @@
 package com.sps.utils;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
 
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.texttospeech.v1.AudioConfig;
 import com.google.cloud.texttospeech.v1.AudioEncoding;
 import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
 import com.google.cloud.texttospeech.v1.SynthesisInput;
 import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
 import com.google.cloud.texttospeech.v1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1.TextToSpeechSettings;
 import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
 
 public class PoemTextToSpeech {
   public byte[] speakPoem(String poemText) throws IOException {
-    TextToSpeechClient textToSpeechClient = TextToSpeechClient.create();
+    InputStream jsonPath = this.getClass().getResourceAsStream("/spring21-sps-4-credentials.json");
+    GoogleCredentials credentials = GoogleCredentials.fromStream(jsonPath);
+    TextToSpeechSettings textToSpeechSettings = TextToSpeechSettings.newBuilder()
+        .setCredentialsProvider(FixedCredentialsProvider.create(credentials)).build();
+    TextToSpeechClient textToSpeechClient = TextToSpeechClient.create(textToSpeechSettings);
     // Build speech synthesis input using poem text
     SynthesisInput input = SynthesisInput.newBuilder().setText(poemText).build();
     VoiceSelectionParams voice = VoiceSelectionParams.newBuilder().setLanguageCode("en-US")
@@ -21,6 +30,14 @@ public class PoemTextToSpeech {
     // Set audio type to MP3 and return speech utterance
     AudioConfig audioConfig = AudioConfig.newBuilder().setAudioEncoding(AudioEncoding.MP3).build();
     SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+    try {
+      textToSpeechClient.shutdown();
+      textToSpeechClient.awaitTermination(30, TimeUnit.SECONDS);
+      textToSpeechClient.close();
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
     return response.getAudioContent().toByteArray();
   }
 }
